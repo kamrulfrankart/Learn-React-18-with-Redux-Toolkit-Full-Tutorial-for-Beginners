1a1d8f2436d897966970383048873d7224b95055
kamrul <kamrul@asabd.org>
2024-04-01 10:47:22 +0600

    Spread Operator

--- src/index.js
+++ src/index.js
@@ -3,50 +3,40 @@ import ReactDOM  from "react-dom/client"
 
 import './index.css'
 
-const firstBook = {
-   author : 'Dav Pilkey',
-   title : 'Dog Man: The Scarlet Shedder: A Graphic Novel',
-   img : './images/book-1.jpg'  
-};
-
-
-const secondBook = {
-  author : 'Jonathan Haidt',
-  title : 'The Anxious Generation: How the Great Rewiring of Childhood Is Causing an Epidemic of Mental Illness',
-  img : './images/book-2.jpg'  
-};
+const books = [
+  {
+    author : 'Dav Pilkey',
+    title : 'Dog Man: The Scarlet Shedder: A Graphic Novel',
+    img : './images/book-1.jpg',
+    id: 1  
+  },
+  {
+    author : 'Jonathan Haidt',
+    title : 'The Anxious Generation: How the Great Rewiring of Childhood Is Causing an Epidemic of Mental Illness',
+    img : './images/book-2.jpg',
+    id: 2
+  },
+];
 
 
 const BookList = () => {
   return (
   <section className="booklist">
-    <Book
-     author={firstBook.author} 
-     title={firstBook.title} 
-     img = {firstBook.img}
-     >     
-     <p>Our canine superhero returns in DOG MAN: THE SCARLET SHEDDER, the suspenseful and hilarious twelfth graphic novel in the #1 worldwide bestselling series by award-winning author and illustrator Dav Pilkey!</p>
-     <button>click</button>
-     </Book>
-    <Book 
-    author={secondBook.author}
-     title={secondBook.title} 
-     img = {secondBook.img}
-     />
+    {books.map((book) => {
+      return <Book {...book} key = {book.id}/>;
+    })}
   </section>
   );
 };
 
 
 const Book = (props) => {
-  const {img, title, author, children} = props;
-  console.log(props);
+  const {img, title, author} = props;
   return (
   <article  className="book">
       <img src= {img} alt = {title}/>
       <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
-      {children}
+      <h4>{author}</h4>
   </article>
   );
 };
