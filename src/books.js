33c66f01b6b0d2c066026994924e11e7236d45e1
kamrul <kamrul@asabd.org>
2024-04-02 12:45:39 +0600

    local image setup

--- src/books.js
+++ src/books.js
@@ -1,15 +1,25 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
 export const books = [
   {
     author: "Dav Pilkey",
     title: "Dog Man: The Scarlet Shedder: A Graphic Novel",
-    img: "./images/book-1.jpg",
+    img: img1,
     id: 1,
   },
   {
     author: "Jonathan Haidt",
     title:
       "The Anxious Generation: How the Great Rewiring of Childhood Is Causing an Epidemic of Mental Illness",
-    img: "./images/book-2.jpg",
+    img: img2,
     id: 2,
+  },
+  {
+    author: "Rebecca Yarros",
+    title: "Onyx Storm (Deluxe Limited Edition) (The Empyrean, 3)",
+    img: img3,
+    id: 3,
   },
 ];
